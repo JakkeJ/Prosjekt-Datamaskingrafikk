7a557730e2cbd591a702fd0f92f36a2ca450568d
Hogne <hsu014@post.uit.no>
2023-11-10 14:18:55 +0100

    Ammo physics working  :)

--- src/gruppe5/script.js
+++ src/gruppe5/script.js
@@ -7,8 +7,29 @@ const ri = {
     currentlyPressedKeys: [],
 }
 
+let phy = {
+    rigidBodies: [],
+
+}
+
 
 export function main() {
+    createThreeScene();
+    createAmmoWorld();
+
+    // Clock for animation
+    ri.clock = new THREE.Clock();
+
+    // Event listeners
+    window.addEventListener('resize', onWindowResize, false);
+    document.addEventListener('keyup', handleKeyUp, false);
+    document.addEventListener('keydown', handleKeyDown, false);
+
+    addToScene();
+}
+
+
+function createThreeScene() {
     const canvas = document.createElement('canvas');
     document.body.appendChild(canvas);
 
@@ -22,21 +43,25 @@ export function main() {
 
     ri.lilGui = new GUI();
 
-    // Clock for animation
-    ri.clock = new THREE.Clock();
-
     ri.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    ri.camera.position.set( 10, 2, 5 );
+    ri.camera.position.set( 20, 10, 10 );
     ri.camera.lookAt( 0, 0, 0 );
-    ri.armCamera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
     ri.controls = new OrbitControls(ri.camera, ri.renderer.domElement);
+}
 
-    // Event listeners
-    window.addEventListener('resize', onWindowResize, false);
-    document.addEventListener('keyup', handleKeyUp, false);
-    document.addEventListener('keydown', handleKeyDown, false);
 
-    addToScene()
+// Hentet fra kodeeksempel modul7/ammoShapes1
+function createAmmoWorld() {
+    phy.transform = new Ammo.btTransform();
+
+    // Initialiserer phy.ammoPhysicsWorld:
+    let collisionConfiguration = new Ammo.btDefaultCollisionConfiguration(),
+        dispatcher = new Ammo.btCollisionDispatcher(collisionConfiguration),
+        overlappingPairCache = new Ammo.btDbvtBroadphase(),
+        solver = new Ammo.btSequentialImpulseConstraintSolver();
+
+    phy.ammoPhysicsWorld = new Ammo.btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+    phy.ammoPhysicsWorld.setGravity(new Ammo.btVector3(0, -9.81, 0));
 }
 
 
@@ -68,7 +93,12 @@ function addToScene() {
     ri.textures.johnny = loader.load('static/johnny.png');
 
     manager.onLoad = () => {
-        ground()
+        addLights()
+
+        // Three-Ammo objects:
+        threeAmmoObjects()
+
+        // Start animate loop
         animate(0)
     }
 }
@@ -81,11 +111,118 @@ function animate(currentTime) {
     // Time interval for smooth movement regardless of FPS:
     let delta = ri.clock.getDelta();
 
+    updatePhysics(delta)
+
     renderCamera()
 
 }
 
 
+function addLights() {
+    ambientLight();
+    directionalLight();
+}
+
+
+// Kode hentet fra oblig 3
+function ambientLight() {
+    ri.ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+    ri.ambientLight.visible = true;
+    ri.scene.add(ri.ambientLight);
+
+    // lilGui
+    ri.ambientFolder = ri.lilGui.addFolder( 'Ambient Light' );
+    ri.ambientFolder.close();
+    ri.ambientFolder.add(ri.ambientLight, 'visible').name("On/Off");
+    ri.ambientFolder.add(ri.ambientLight, 'intensity').min(0).max(1).step(0.01).name("Intensity");
+    ri.ambientFolder.addColor(ri.ambientLight, 'color').name("Color");
+}
+
+
+// Kode hentet fra oblig 3
+function directionalLight() {
+    ri.directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    ri.directionalLight.position.set(1, 20, 0);
+    ri.directionalLight.castShadow = true;
+
+    ri.directionalLight.shadow.mapSize.width = 1024;
+    ri.directionalLight.shadow.mapSize.height = 1024;
+    ri.directionalLight.shadow.camera.near = 0;
+    ri.directionalLight.shadow.camera.far = 50;
+    ri.directionalLight.shadow.camera.left = -15;
+    ri.directionalLight.shadow.camera.right = 15;
+    ri.directionalLight.shadow.camera.top = 15;
+    ri.directionalLight.shadow.camera.bottom = -15;
+
+    const directionalLightHelper = new THREE.DirectionalLightHelper(ri.directionalLight, 10, 0xff7777);
+    const directionalLightCameraHelper = new THREE.CameraHelper(ri.directionalLight.shadow.camera)
+    directionalLightHelper.visible = false;
+    directionalLightCameraHelper.visible = false;
+
+    ri.scene.add(ri.directionalLight);
+    ri.scene.add(directionalLightHelper);
+    ri.scene.add(directionalLightCameraHelper);
+
+    // Add folder to lilGui
+    ri.directionalFolder = ri.lilGui.addFolder('Directional Light');
+    ri.directionalFolder.close();
+    ri.directionalFolder.add(ri.directionalLight, 'visible').name("On/Off");
+
+    ri.directionalFolder.add(directionalLightHelper, 'visible').name("Helper").onChange(value => {
+        directionalLightCameraHelper.visible = value;
+    });
+    ri.directionalFolder.add(ri.directionalLight, 'intensity').min(0).max(1).step(0.01).name("Intensity");
+    ri.directionalFolder.addColor(ri.directionalLight, 'color').name("Color");
+}
+
+
+// Hentet fra kodeeksempel modul7/ammoShapes1
+function createAmmoRigidBody(shape, threeMesh, restitution=0.7, friction=0.8, position={x:0, y:50, z:0}, mass=1, useLocalScaling=true) {
+
+    let transform = new Ammo.btTransform();
+    transform.setIdentity();
+    transform.setOrigin(new Ammo.btVector3(position.x, position.y, position.z));
+
+    let quaternion = threeMesh.quaternion;
+    transform.setRotation(new Ammo.btQuaternion(quaternion.x, quaternion.y, quaternion.z, quaternion.w));
+
+    if (useLocalScaling) {
+        let scale = threeMesh.scale;
+        shape.setLocalScaling(new Ammo.btVector3(scale.x, scale.y, scale.z));
+    }
+
+    let motionState = new Ammo.btDefaultMotionState(transform);
+    let localInertia = new Ammo.btVector3(0, 0, 0);
+    shape.calculateLocalInertia(mass, localInertia);
+
+    let rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, shape, localInertia);
+    let rigidBody = new Ammo.btRigidBody(rbInfo);
+    rigidBody.setRestitution(restitution);
+    rigidBody.setFriction(friction);
+
+    return rigidBody;
+}
+
+
+// Hentet fra kodeeksempel modul7/ammoShapes1
+function updatePhysics(deltaTime) {
+    phy.ammoPhysicsWorld.stepSimulation(deltaTime, 10);
+    for (let i = 0; i < phy.rigidBodies.length; i++) {
+        let mesh = phy.rigidBodies[i];
+        let rigidBody = mesh.userData.physicsBody;
+        let motionState = rigidBody.getMotionState();
+        if (motionState) {
+            motionState.getWorldTransform(phy.transform);
+            let p = phy.transform.getOrigin();
+            let q = phy.transform.getRotation();
+            mesh.position.set(p.x(), p.y(), p.z());
+            mesh.quaternion.set(q.x(), q.y(), q.z(), q.w());
+        }
+    }
+}
+
+
+// Kode hentet fra oblig 3
 function createMesh(geometry, material, parent, name = "", translateY = 0, translateZ = 0, rotateX = 0, rotateY = 0, rotateZ = 0, scaleX = 1, scaleY = 1, scaleZ = 1) {
     const mesh = new THREE.Mesh(geometry, material);
 
@@ -103,14 +240,63 @@ function createMesh(geometry, material, parent, name = "", translateY = 0, trans
 }
 
 
+function threeAmmoObjects() {
+    ground()
+
+    const ballPosition = {x: 0, y: 20, z: 0};
+    const ballRadius = 2
+    const ballMass = 10
+    ball(ballPosition, ballRadius, ballMass)
+}
+
+
 function ground() {
-    const geometry = new THREE.BoxGeometry(50, 1, 50);
-    const groundTexture = new THREE.MeshBasicMaterial({map: ri.textures.johnny, side: THREE.DoubleSide});
-    const mesh = new THREE.Mesh(geometry, groundTexture);
+    const position = {x: 0, y: 0, z: 0};
+    const size = {x: 50, y:1, z: 50};
+
+    // THREE
+    const geometry = new THREE.BoxGeometry(size.x, size.y, size.z);
+    const material = new THREE.MeshStandardMaterial({map: ri.textures.johnny, side: THREE.DoubleSide});
+    const mesh = new THREE.Mesh(geometry, material);
 
     mesh.name = 'ground';
     //mesh.castShadow = true;
     mesh.receiveShadow = true;
 
     ri.scene.add(mesh);
+
+    // AMMO
+    let shape = new Ammo.btBoxShape(new Ammo.btVector3(size.x/2, size.y/2, size.z/2));
+    let rigidBody = createAmmoRigidBody(shape, mesh, 0.7, 0.8, position, 0);
+
+    mesh.userData.physicsBody = rigidBody;
+
+    phy.ammoPhysicsWorld.addRigidBody(rigidBody);  // Legger til ammo physics world:
+    phy.rigidBodies.push(mesh);
+    rigidBody.threeMesh = mesh; //Brukes til collision events:
+}
+
+
+function ball(position, radius, mass) {
+    // THREE
+    const geometry = new THREE.SphereGeometry(radius, 32, 32);
+    const material = new THREE.MeshStandardMaterial({color: 0xFFFFFF});
+    const mesh = new THREE.Mesh(geometry, material);
+
+    mesh.name = 'ball';
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    mesh.position.set(position.x, position.y, position.z);
+
+    ri.scene.add(mesh);
+
+    // AMMO
+    let shape = new Ammo.btSphereShape(radius);
+    let rigidBody = createAmmoRigidBody(shape, mesh, 0.7, 0.8, position, mass);
+
+    mesh.userData.physicsBody = rigidBody;
+
+    phy.ammoPhysicsWorld.addRigidBody(rigidBody);  // Legger til ammo physics world:
+    phy.rigidBodies.push(mesh);
+    rigidBody.threeMesh = mesh;
 }
